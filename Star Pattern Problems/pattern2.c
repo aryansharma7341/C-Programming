e5b031310086a98874b94fed25ad6d515ece605c
Aryan Sharma <113662412+Aryan7341@users.noreply.github.com>
2022-11-13 01:17:50 +0530

    Add files via upload

---
+++ Star Pattern Problems/pattern2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(5-i>=j)
+            {
+                printf(" ");
+            }
+            else
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
